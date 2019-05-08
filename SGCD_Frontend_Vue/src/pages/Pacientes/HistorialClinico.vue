5aa06fdf5b30df33e5181548de233cd76a4c4585
carver300 <penunurisantiago@gmail.com>
2019-05-08 09:59:26 -0600

    Historial Pacientes 70%

--- SGCD_Frontend_Vue/src/pages/Pacientes/HistorialClinico.vue
+++ SGCD_Frontend_Vue/src/pages/Pacientes/HistorialClinico.vue
@@ -2,18 +2,19 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-xl-12">
+                <div class="col-lg-12 col-xl-12">
                     <card>
                         <div class="row">
-                            <div class="col-xl-2">
+                            <div class="col-xl-3">
                                 <card>
                                     <div slot="header">
-                                        <h3>Buscar Paciente</h3>
+                                        <h3>Paciente</h3>
                                     </div>
                                     <div class="row">
-                                        <div class="col-xl-12">
+                                        <div class="col-lg-8 col-xl-12">
                                             <base-input
                                                 label="Paciente"
+                                                placeholder="Numero Paciente"
                                                 >
                                             </base-input>
                                             <button class="btn btn-fill btn-info">
@@ -21,12 +22,9 @@
                                             </button>
                                         </div>
                                     </div>
-                                    <div slot="footer">
-
-                                    </div>
                                 </card>
                             </div>
-                            <div class="col-xl-10">
+                            <div class="col-xl-9">
                                 <card>
                                     <div slot="header">
                                         <h3>Informacion Paciente</h3>
@@ -63,9 +61,70 @@
                                 </card>
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-10 col-xl-12">
+                               <card>
+                                   <h3>Historial</h3>
+                                   <div class="row">
+                                       <div class="col-lg-12 col-xl-12">
+                                           <div class="table-responsive">
+                                               <l-table
+                                            class="table-hover"
+                                            :colums="tableColums"
+                                            :data="tableData">
+                                                <template slot="columns"></template>
+                                                <template slot-scope="{row}">
+                                                    <td>{{row.id_paciente}}</td>
+                                                    <td>{{row.nombre}}</td>
+                                                    <td>{{row.apepaterno}}</td>
+                                                    <td>{{row.apematerno}}</td>
+                                                    <td>{{row.sexo}}</td>
+                                                    <td>{{row.edad}}</td>
+                                                    <td>{{row.alergia}}</td>
+                                                    <td>{{row.calle}}</td>
+                                                    <td>{{row.colonia}}</td>
+                                                    <td>{{row.codigopostal}}</td>
+                                                    <td>{{row.telefono}}</td>
+                                                    <td>{{row.correo}}</td>
+                                                    <td>
+                                                        <button class="btn btn-icon btn-info" @click="cargarDatosModal(row)"><i class="fa fa-edit"></i></button>
+                                                        <button class="btn btn-icon btn-danger" @click="eliminarPaciente(row)"><i class="fa fa-trash"></i></button>
+                                                    </td>
+                                                </template>
+                                           </l-table>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </card>
+                            </div>
+                        </div>
                     </card>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+
+<script>
+import LTable from 'src/components/Table.vue'
+import Card from 'src/components/Cards/Card.vue'
+const axios = require('axios');
+
+export default {
+    name:'InformacionPacientes',
+    components: {
+      LTable,
+      Card
+    },
+    data(){
+            return {
+                    tableColums : ['Id', 'Nombre', 'Apellido Paterno', 'Apellido Materno', 'Sexo', 'Edad' ,
+                    'Alergia' , 'Calle' , 'Colonia' , 'Codigo Postal' , 'Telefono' , 'Correo Electronico','Acciones'],
+                     tableData:[],
+                                      }
+         }
+
+}
+</script>
+    
