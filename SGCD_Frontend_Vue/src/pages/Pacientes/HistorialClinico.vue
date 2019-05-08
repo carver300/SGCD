1d4b572527dc141127a38c4a5f57f885bee9f174
SANTIAGO PEÑUÑURI QUINTERO <14170969@itculiacan.edu.mx>
2019-05-08 01:18:26 -0500

    Historial Pacientes 30%

--- SGCD_Frontend_Vue/src/pages/Pacientes/HistorialClinico.vue
+++ SGCD_Frontend_Vue/src/pages/Pacientes/HistorialClinico.vue
@@ -5,25 +5,61 @@
                 <div class="col-xl-12">
                     <card>
                         <div class="row">
-                            <div class="col-xl-6">
+                            <div class="col-xl-2">
                                 <card>
+                                    <div slot="header">
+                                        <h3>Buscar Paciente</h3>
+                                    </div>
                                     <div class="row">
-                                        <h3>Paciente</h3>
-                                        <div class="col-xl-6">
+                                        <div class="col-xl-12">
                                             <base-input
                                                 label="Paciente"
                                                 >
                                             </base-input>
-                                            <button>
-                                                amarillo
+                                            <button class="btn btn-fill btn-info">
+                                                Buscar
                                             </button>
                                         </div>
                                     </div>
+                                    <div slot="footer">
+
+                                    </div>
                                 </card>
                             </div>
-                            <div class="col-xl-6">
+                            <div class="col-xl-10">
                                 <card>
-                                    <h1>hola</h1>
+                                    <div slot="header">
+                                        <h3>Informacion Paciente</h3>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xl-3">
+                                            <base-input
+                                                label="Nombre">
+                                            </base-input>
+                                        </div>
+                                        <div class="col-xl-3">
+                                            <base-input
+                                                label="Apellido Paterno">
+                                            </base-input>
+                                        </div>
+                                        <div class="col-xl-3">
+                                            <base-input
+                                                label="Apellido Materno">
+                                            </base-input>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xl-3">
+                                            <base-input
+                                                label="Sexo">
+                                            </base-input>
+                                        </div>
+                                        <div class="col-xl-2">
+                                            <base-input
+                                                label="Edad">
+                                            </base-input>
+                                        </div>
+                                    </div>
                                 </card>
                             </div>
                         </div>
