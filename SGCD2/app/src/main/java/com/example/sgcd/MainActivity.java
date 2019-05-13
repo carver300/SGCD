519382df147070ab67968351c926224a972b7578
SANTIAGO PEUURI QUINTERO <14170969@itculiacan.edu.mx>
2019-05-13 09:51:59 -0600

    Boton Registrar

--- SGCD2/app/src/main/java/com/example/sgcd/MainActivity.java
+++ SGCD2/app/src/main/java/com/example/sgcd/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sgcd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        TextView text = findViewById(R.id.txtEjemplo);
+
+        text.setText("Diste un click");
     }
 }
