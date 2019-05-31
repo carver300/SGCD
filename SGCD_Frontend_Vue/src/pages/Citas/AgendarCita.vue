4dc1b658e7667bc8785885d7dc459b651bb10092
carver300 <penunurisantiago@gmail.com>
2019-05-31 04:10:36 -0500

    Se agrega atencion de clientes y venta

--- SGCD_Frontend_Vue/src/pages/Citas/AgendarCita.vue
+++ SGCD_Frontend_Vue/src/pages/Citas/AgendarCita.vue
@@ -105,16 +105,142 @@
                 </select>
               </div>
               <div class="col-xl-2 pt-4">
-                <button type="submit" class="btn btn-fill btn-success" @click="abrirModalCobrar()">Cobrar</button>
+                <button
+                  type="submit"
+                  class="btn btn-fill btn-success"
+                  @click="abrirModalCobrar()"
+                >Cobrar</button>
               </div>
             </div>
           </card>
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-xl-2">
+        <div v-if="showModal">
+          <transition name="modal">
+            <div class="modal-mask">
+              <div class="modal-wrapper">
+                <div class="modal-dialog modal-lg" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <div class="row justify-content-center">
+                        <div class="col-xl-12">
+                          <h3>Editar Informacion</h3>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal-body">
+                      <div class="row">
+                        <div class="col-xl-3">
+                          <base-input
+                            type="text"
+                            label="Nombre"
+                            placeholder="Nombre del paciente"
+                            v-model="objetoPaciente.nombre"
+                          ></base-input>
+                        </div>
+                        <div class="col-xl-3">
+                          <base-input
+                            type="text"
+                            label="Apellido Paterno"
+                            placeholder="Primer apellido"
+                            v-model="objetoPaciente.apepaterno"
+                          ></base-input>
+                        </div>
+                        <div class="col-xl-3">
+                          <base-input
+                            type="text"
+                            label="Apellido Materno"
+                            placeholder="Segundo apellido"
+                            v-model="objetoPaciente.apematerno"
+                          ></base-input>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-xl-3">
+                          <base-input
+                            type="text"
+                            label="Sexo"
+                            placeholder="Sexo"
+                            v-model="objetoPaciente.sexo"
+                          ></base-input>
+                        </div>
+                        <div class="col-xl-3">
+                          <base-input type="text" label="Edad" v-model="objetoPaciente.edad"></base-input>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-xl-4">
+                          <div class="form-group">
+                            <label>Alergias</label>
+                            <textarea
+                              rows="3"
+                              class="form-control border-input"
+                              placeholder="Alergia a medicamentos"
+                              v-model="objetoPaciente.alergia"
+                            ></textarea>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-xl-3">
+                          <base-input type="text" label="Calle" v-model="objetoPaciente.calle"></base-input>
+                        </div>
+                        <div class="col-xl-3">
+                          <base-input type="text" label="Colonia" v-model="objetoPaciente.colonia"></base-input>
+                        </div>
+                        <div class="col-xl-3">
+                          <base-input
+                            type="number"
+                            label="Codigo Postal"
+                            v-model="objetoPaciente.codigopostal"
+                          ></base-input>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button
+                        type="submit"
+                        class="btn btn-info btn-fill float-right"
+                        @click="showModal = false"
+                      >Cerrar</button>
+                      <button
+                        type="submit"
+                        class="btn btn-success btn-fill float-right"
+                        @click="editarInformacion()"
+                      >Editar</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </transition>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+    .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        display: table;
+        transition: opacity .3s ease;
+    }
+
+    .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+</style>
 
 <script>
 const axios = require("axios");
@@ -147,6 +273,7 @@ export default {
         precio: "",
         tiempoestimado: ""
       },
+      showModal: false,
       serv: 1,
       hor: 1,
       opcion: "",
@@ -158,6 +285,20 @@ export default {
         fecha: "",
         hora: "",
         observaciones: ""
+      },
+      objetoPaciente: {
+        id_paciente: "",
+        nombre: "",
+        apepaterno: "",
+        apematerno: "",
+        sexo: "",
+        edad: "",
+        calle: "",
+        colonia: "",
+        codigopostal: "",
+        telefono: "",
+        correo: "",
+        alergia: ""
       }
     };
   },
@@ -228,6 +369,10 @@ export default {
             "danger"
           );
         });
+    },
+
+    abrirModalCobrar() {
+      this.showModal = true;
     },
 
     agendarCita() {
--- SGCD_Frontend_Vue/src/pages/Citas/AtencionDeCita.vue
+++ SGCD_Frontend_Vue/src/pages/Citas/AtencionDeCita.vue
@@ -32,9 +32,173 @@
               <h3 class="card-title">Informacion del paciente</h3>
               <p class="card-category">Informacion general del paciente</p>
             </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-xl-3">
+                  <base-input label="Nombre" v-model="Paciente.nombre"></base-input>
+                </div>
+                <div class="col-xl-3">
+                  <base-input label="Apellido Paterno" v-model="Paciente.apepaterno"></base-input>
+                </div>
+                <div class="col-xl-3">
+                  <base-input label="Apellido Materno" v-model="Paciente.apematerno"></base-input>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xl-3">
+                  <base-input label="Sexo" v-model="Paciente.sexo"></base-input>
+                </div>
+                <div class="col-xl-2">
+                  <base-input label="Edad" v-model="Paciente.edad"></base-input>
+                </div>
+                <div class="col-xl-4">
+                  <label>Alergias</label>
+                  <textarea rows="1" class="form-control border-input" v-model="Paciente.alergia"></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xl-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Datos de la cita</h3>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-xl-2">
+                  <base-input label="Folio Cita" v-model="Cita.id_cita" :disabled="true"></base-input>
+                </div>
+                <div class="col-xl-2">
+                  <base-input label="No. Paciente" v-model="Cita.id_paciente" :disabled="true"></base-input>
+                </div>
+                <div class="col-xl-2">
+                  <base-input label="No.Servicio" v-model="Cita.id_servicio" :disabled="true"></base-input>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xl-2">
+                  <base-input label="Fecha" v-model="Cita.fecha" :disabled="true"></base-input>
+                </div>
+                <div class="col-xl-2">
+                  <base-input label="Hora" v-model="Cita.hora" :disabled="true"></base-input>
+                </div>
+                <div class="col-xl-2">
+                  <base-input label="Estatus" v-model="Cita.estatus" :disabled="true"></base-input>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xl-4">
+                  <div class="form-group">
+                    <label>Observaciones</label>
+                    <textarea
+                      rows="3"
+                      class="form-control border-input"
+                      placeholder="Observaciones o anotaciones de la cita"
+                      v-model="Cita.observaciones"
+                    ></textarea>
+                  </div>
+                </div>
+                <div class="col-xl-3 pt-4 d-flex flex-wrap align-content-center">
+                  <button class="btn btn-fill btn-info">Finalizar Cita</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+
+<script>
+const axios = require("axios");
+
+export default {
+  data() {
+    return {
+      Paciente: {
+        nombre: "",
+        apepaterno: "",
+        apematerno: "",
+        sexo: "",
+        edad: "",
+        calle: "",
+        colonia: "",
+        codigopostal: "",
+        telefono: "",
+        correo: "",
+        alergia: ""
+      },
+      Cita: {
+        id_cita: "",
+        id_paciente: "",
+        id_servicio: "",
+        fecha: "",
+        hora: "",
+        estatus: "",
+        observaciones: ""
+      },
+      FolioCita: ""
+    };
+  },
+  methods: {
+    traerInformacionCita() {
+      axios
+        .get(
+          "https://SGCD.azurewebsites.net/api/Cita/CitaPorId" + this.FolioCita
+        )
+        .then(response => {
+          if (response.data != -1) {
+            this.Cita = response.data;
+            this.buscarInformacionPaciente();
+          }
+        })
+        .catch(error => {
+          alert(error);
+        });
+    },
+
+    buscarInformacionPaciente() {
+      if (this.idPaciente > 0 && this.idPaciente != "") {
+        axios
+          .get(
+            "https://SGCD.azurewebsites.net/api/paciente/PacientePorID/" +
+              this.Cita.id_paciente
+          )
+          .then(response => {
+            if (response.data != -1) {
+              this.Paciente = response.data;
+              this.obtenerHistorialCitas();
+            } else {
+              this.notifyVue(
+                "top",
+                "center",
+                "No hay informacion para ese paciente",
+                "danger"
+              );
+            }
+          })
+          .catch(error => {
+            this.notifyVue(
+              "top",
+              "center",
+              "Hubo un error al obtener la informacion, favor de reportarlo con el administrador",
+              "danger"
+            );
+          });
+      } else {
+        this.notifyVue(
+          "top",
+          "center",
+          "No haz capturado un numero de paciente valido",
+          "warning"
+        );
+      }
+    }
+  }
+};
+</script>
